3feb92629bdfae4792e4436053fb347dad6f82f6
Satoshi Robato Fujimoto <good_2_go@live.jp>
2015-07-27 02:46:09 +0900

    vtk library dependencies

---
+++ opencv_vtk_lib.hpp
@@ -0,0 +1,44 @@
+// ビルドモード
+#ifdef _DEBUG
+#define CV_VTK_EXT_STR "-gd.lib"
+#else
+#define CV_VTK_EXT_STR ".lib"
+#endif
+
+#pragma comment(lib, "LSDyna" CV_VTK_EXT_STR)
+#pragma comment(lib, "MapReduceMPI" CV_VTK_EXT_STR)
+#pragma comment(lib, "mpistubs" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkalglib" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkCharts" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkCommon" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkDICOMParser" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkexoIIc" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkexpat" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkFiltering" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkfreetype" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkftgl" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkGenericFiltering" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkGeovis" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkGraphics" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkhdf5" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkhdf5_hl" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkHybrid" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkImaging" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkInfovis" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkIO" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkjpeg" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtklibxml2" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkmetaio" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkNetCDF" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkpng" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkproj4" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkRendering" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtksqlite" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtksys" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtktiff" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkverdict" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkViews" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkVolumeRendering" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkWidgets" CV_VTK_EXT_STR)
+#pragma comment(lib, "vtkzlib" CV_VTK_EXT_STR)
+
